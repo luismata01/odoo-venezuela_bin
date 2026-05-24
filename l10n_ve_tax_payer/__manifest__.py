b7aef68983b6100729262baf4269261b0bb8aabc
luismata01 <luis.mata01@gmail.com>
2026-05-24 09:30:16 -0400

    [MIG] l10n_ve_tax_payer: migrate to Odoo 19.0
    - Update version in manifest: 1.0 -> 19.0.1.0.0

--- l10n_ve_tax_payer/__manifest__.py
+++ l10n_ve_tax_payer/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "1.0",
+    "version": "19.0.1.0.0",
     "depends": ["base", "l10n_ve_rate", "l10n_ve_accountant"],
     "data": [
         "views/res_partner.xml",
