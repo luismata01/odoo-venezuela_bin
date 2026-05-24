526e1cffeec2ae1a3d5f83f349bc2977f3ff584a
luismata01 <luis.mata01@gmail.com>
2026-05-24 14:40:53 -0400

    [MIG] l10n_ve_auditlog: migrate to Odoo 19.0
    - Update version in manifest to 19.0.1.0.0
    - Replace deprecated <tree> view tag with <list>
    - Adapt mail.tracking.value views to Odoo 19 field changes:
      * Remove non-existent fields: model, field, author_id, field_type,
        new_value_monetary, old_value_monetary
      * Add available fields: field_id, mail_message_id, currency_id
      * Update action domain to use mail_message_id.model

--- l10n_ve_auditlog/__manifest__.py
+++ l10n_ve_auditlog/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Technical",
-    "version": "1.0",
+    "version": "19.0.1.0.0",
     "data": [
         "views/auditlog_lines_views.xml",
         "views/auditlogs_views.xml",
