d681cbf5602bcc9e8944722f1c92c5228e200375
luismata01 <luis.mata01@gmail.com>
2026-06-17 08:00:18 -0400

    [FIX] l10n_ve_binaural: fix env.ref xmlid format in demo; l10n_ve_igtf: fix self vs rec in compute_bi_igtf

--- l10n_ve_igtf/models/account_move.py
+++ l10n_ve_igtf/models/account_move.py
@@ -734,7 +734,7 @@ class AccountMove(models.Model):
     def compute_bi_igtf(self):
         for rec in self:
             if abs(rec.amount_total_signed) > 0 or rec.payment_state in ['paid','in_payment']: 
-                rec.igtf_top_aply = abs(rec.amount_total_signed) * (self.company_id.igtf_percentage / 100)
+                rec.igtf_top_aply = abs(rec.amount_total_signed) * (rec.company_id.igtf_percentage / 100)
                 receivable_payable_lines = rec.line_ids.filtered(lambda line: line.account_id.reconcile)
 
                 final_payment_moves = receivable_payable_lines.reconciled_lines_excluding_exchange_diff_ids.mapped('move_id')
